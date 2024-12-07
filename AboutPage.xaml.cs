8737315799f3f01d99f821f3513a3d3650dbeb85
Dana2611 <dana.margineanu3@yahoo.com>
2024-12-07 10:47:25 +0200

    Add project files.

---
+++ AboutPage.xaml.cs
@@ -0,0 +1,9 @@
+namespace MargineanuDanaLab7;
+
+public partial class AboutPage : ContentPage
+{
+	public AboutPage()
+	{
+		InitializeComponent();
+	}
+}
---
+++ App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace MargineanuDanaLab7
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
