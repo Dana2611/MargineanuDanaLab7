afd6d587a974348142e9ba001b20858929fbe666
Dana2611 <dana.margineanu3@yahoo.com>
2024-12-07 11:40:59 +0200

    Laborator8

--- App.xaml.cs
+++ App.xaml.cs
@@ -1,7 +1,24 @@
-﻿namespace MargineanuDanaLab7
+﻿using MargineanuDanaLab7.Data;
+
+namespace MargineanuDanaLab7
 {
     public partial class App : Application
     {
+        static ShoppingListDatabase database;
+        public static ShoppingListDatabase Database
+        {
+            get
+            {
+                if (database == null)
+                {
+                    database = new
+                   ShoppingListDatabase(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.
+                   LocalApplicationData), "ShoppingList.db3"));
+                }
+                return database;
+            }
+        }
+
         public App()
         {
             InitializeComponent();
